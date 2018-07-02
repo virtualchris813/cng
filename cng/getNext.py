1554a73f50112c846bb663feee00c580ec488575
Chris Adams <virtualchris813@gmail.com>
2018-07-01 22:22:01 -0400

    Update getNext.py
    Added some comments

--- cng/getNext.py
+++ cng/getNext.py
@@ -1,7 +1,10 @@
 from string import ascii_lowercase
 import itertools
 letters = ascii_lowercase
-noz = letters.replace("z","")
+# Example of skipping z, could be modified for your specific needs.
+# This makes the iterator go from a,b..x,y,aa,ab..ax,ay,ba (no z is used)
+skip_letter = "z"
+noz = letters.replace(skip_letter,"")
 
 def iter_all_strings():
   for size in itertools.count(1):
@@ -10,10 +13,14 @@ def iter_all_strings():
 
 
 def getNextLetter(currentLetter):
+  if currentLetter == skip_letter:
+    raise ValueError, 'Current Letter is the Skip Letter. Something is messed up! Exiting.'
   last_one = False
   for s in iter_all_strings():
     if last_one == False:
       if s == currentLetter:
+        # If we are on the current iteration, set last_one to True
+        # so we can exit after next loop iteration.
         last_one = True
     else:
       return s
